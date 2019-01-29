f652aeb34fd237c95a7039002b17072b7670da26
Satish Logade <satish.logade@globallogic.com>
2019-01-29 18:48:39 +0530

    Indentation changes

--- assignment6/src/components/ControlBar.tsx
+++ assignment6/src/components/ControlBar.tsx
@@ -5,69 +5,71 @@ import DatePicker from 'react-datepicker'
 import { GetSelectedDateEventsAction } from './../actions/events'
 
 interface Props {
-  dateChange: (date: String) => GetSelectedDateEventsAction
+    dateChange: (date: String) => GetSelectedDateEventsAction
 }
 
 interface State {
-  date: Date
+    date: Date
 }
 
 export default class ControlBar extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
+    constructor(props: Props) {
+        super(props)
 
-    this.state = {
-      date: new Date()
+        this.state = {
+            date: new Date()
+        }
     }
-  }
 
-  increment = () => {
-    let {date} = this.state
-    this.setState({date: new Date(date.getFullYear(), date.getMonth(), (date.getDate() + 1))},
-      () => this.props.dateChange(this.state.date.toDateString())
-    )
-  }
+    increment = () => {
+        let { date } = this.state
+        this.setState({ date: new Date(date.getFullYear(), date.getMonth(), (date.getDate() + 1)) },
+            () => this.props.dateChange(this.state.date.toDateString())
+        )
+    }
 
-  decrement = () => {
-    let {date} = this.state
-    this.setState({date: new Date(date.getFullYear(), date.getMonth(), (date.getDate() - 1))},
-      () => this.props.dateChange(this.state.date.toDateString())
-    )
-  }
+    decrement = () => {
+        let { date } = this.state
+        this.setState({ date: new Date(date.getFullYear(), date.getMonth(), (date.getDate() - 1)) },
+            () => this.props.dateChange(this.state.date.toDateString())
+        )
+    }
 
-  today = () => {
-    this.setState({date: new Date()}, () => this.props.dateChange(this.state.date.toDateString()))
-  }
+    today = () => {
+        this.setState({ date: new Date() }, () => this.props.dateChange(this.state.date.toDateString()))
+    }
 
-  change = (date: Date) => {
-    this.setState({date}, () => this.props.dateChange(this.state.date.toDateString()))
-  }
+    change = (date: Date) => {
+        this.setState({ date }, () => this.props.dateChange(this.state.date.toDateString()))
+    }
 
-  componentDidMount = () => {
-    this.props.dateChange(this.state.date.toDateString())
-  }
+    componentDidMount = () => {
+        this.props.dateChange(this.state.date.toDateString())
+    }
 
-  render() {
-    return (
-      <div className="control-bar">
-        <div className="left-pannel">
-          <button className="control" onClick={this.today}>Today</button>
-        </div>
-        <div className="date">
-          <button className="control" onClick={this.decrement}>{'<'}</button>
-          <DatePicker
-            selected={this.state.date}
-            onChange={this.change}
-            dateFormat="d MMM, YYYY"
-          />
-          <button className="control" onClick={this.increment}>{'>'}</button>
-        </div>
-        <div className="right-pannel">
-          <Link to={`/add/${this.state.date.toString()}`}>
-            <button className="control">Add</button>
-          </Link>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="control-bar">
+                <div className="left-pannel">
+                    <button className="control" onClick={this.today}>Today</button>
+                </div>
+                <div className="date">
+                    <button className="control" onClick={this.decrement}>{'<'}</button>
+                    <DatePicker
+                        selected={this.state.date}
+                        onChange={this.change}
+                        dateFormat="d MMM, YYYY"
+                        showMonthDropdown
+                        showYearDropdown
+                    />
+                    <button className="control" onClick={this.increment}>{'>'}</button>
+                </div>
+                <div className="right-pannel">
+                    <Link to={`/add/${this.state.date.toString()}`}>
+                        <button className="control">Add</button>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
 }
--- assignment6/src/components/EventDetails.tsx
+++ assignment6/src/components/EventDetails.tsx
@@ -4,39 +4,39 @@ import { GetEventAction } from './../actions/events'
 import Event from './../models/Event'
 
 interface Props {
-  selectedEvent: Event
-  getEvent: (id: number) => GetEventAction,
-  match: any
+    selectedEvent: Event
+    getEvent: (id: number) => GetEventAction,
+    match: any
 }
 export default class EventDetails extends React.Component<Props, any> {
-  constructor(props: any) {
-    super(props)
-  }
-
-  componentDidMount = () => {
-    if (this.props.match.params.id) {
-      this.props.getEvent(this.props.match.params.id)
-    }
-  }
-
-  render() {
-    if (this.props.selectedEvent) {
-      var { title, startTime, endTime, details } = this.props.selectedEvent
-    } else {
-      return <div className="no-events">No Event</div>
+    constructor(props: any) {
+        super(props)
     }
 
-    return (
-      <div className="event-form">
-        <h1 className="heading">{title}</h1>
+    componentDidMount = () => {
+        if (this.props.match.params.id) {
+            this.props.getEvent(this.props.match.params.id)
+        }
+    }
 
-        <button className="cancle" onClick={() => history.back()}>{`Home`}</button>
-        <div className="content">
-          <div className="row">Date: <span>{moment(new Date(startTime)).format('DD-MM-YYYY')}</span></div>
-          <div className="row">Time: <span>{moment(new Date(startTime)).format('hh:mm a')} To {moment(new Date(endTime)).format('hh:mm a')}</span></div>
-          <div className="row">Details: <span>{details}</span></div>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        if (this.props.selectedEvent) {
+            var { title, startTime, endTime, details } = this.props.selectedEvent
+        } else {
+            return <div className="no-events">No Event</div>
+        }
+
+        return (
+            <div className="event-form">
+                <h1 className="heading">{title}</h1>
+
+                <button className="cancle" onClick={() => history.back()}>{`Home`}</button>
+                <div className="content">
+                    <div className="row">Date: <span>{moment(new Date(startTime)).format('DD-MM-YYYY')}</span></div>
+                    <div className="row">Time: <span>{moment(new Date(startTime)).format('hh:mm a')} To {moment(new Date(endTime)).format('hh:mm a')}</span></div>
+                    <div className="row">Details: <span>{details}</span></div>
+                </div>
+            </div>
+        )
+    }
 }
